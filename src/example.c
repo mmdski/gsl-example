ec274e28177b61a50aa2eac67b9e2958d2018d2b
Marian Domanski <mmdski@gmail.com>
2020-03-27 18:07:32 -0500

    add glib_dep, example_error

--- src/example.c
+++ src/example.c
@@ -1,6 +1,36 @@
+#include <glib.h>
 #include <gsl/gsl_sf_bessel.h>
 
-double bessel(double x)
+#define EX_EXAMPLE_ERROR ex_example_error_quark ()
+
+enum ExExampleError
 {
-    return gsl_sf_bessel_J0(x);
+  EX_EXAMPLE_ERROR_OCCURED
+};
+
+GQuark
+ex_example_error_quark (void)
+{
+  return g_quark_from_static_string ("ex-example-error-quark");
+}
+
+double
+bessel (double x)
+{
+  return gsl_sf_bessel_J0 (x);
+}
+
+gboolean
+raise_error (GError **error)
+{
+  g_return_val_if_fail (error == NULL || *error == NULL, FALSE);
+
+  if (TRUE)
+    {
+      g_set_error (error, EX_EXAMPLE_ERROR, EX_EXAMPLE_ERROR_OCCURED,
+                   "Example error");
+      return FALSE;
+    }
+  else
+    return TRUE;
 }
